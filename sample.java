d7da8a5fa0fa89f8f744174d6fc78c23a57b1af0
satish-art <84613006+satish-art@users.noreply.github.com>
2022-03-31 13:33:37 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,7 +1,5 @@
-// Your First Program
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
-}
+public class FirstJavaProgram {
+  public static void main(String[] args){
+    System.out.println("This is my first program in java");
+  }//End of main
+}//End of FirstJavaProgram Class
