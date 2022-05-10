5838f790066ce08cc10da7f9a6d2d49d001f344e
satish-art <84613006+satish-art@users.noreply.github.com>
2022-05-10 12:47:35 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,10 +1,5 @@
-
-Timer timer = new Timer();
-timer.schedule(new TimerTask() {
-  @Override
-  public void run() {
-    //what you want to do
-  }
-}, 0, 1000);//wait 0 ms before doing the action and do it evry 1000ms (1second)
-
-timer.cancel();//stop the timer
+public class sample {
+    public static void main(String[] args) {
+        System.out.println("Hello world..!!!");
+    }
+}
